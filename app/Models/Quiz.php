8d482dfc82f4effd12827398fbf16d0327c4f85a
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2025-05-16 11:40:19 +0300

    create table structure

---
+++ app/Models/Quiz.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Quiz extends Model
+{
+    protected $table = 'quizzes';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'title',
+        'icon',
+    ];
+
+    /**
+     * Get the questions for the quizz.
+     */
+    public function questions(): HasMany
+    {
+        return $this->hasMany(Question::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Question;
+use App\Models\Quiz;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +17,32 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $json = File::get(database_path('seeders/data.json'));
+        $quizes = json_decode($json, true);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // dd($quizzes);
+        logger("QUizes", $quizes['quizzes']);
+
+        foreach ($quizes['quizzes'] as $quiz) {
+
+            $quizModel = Quiz::create([
+                'title' => $quiz['title'],
+                'icon' => $quiz['icon'],
+            ]);
+
+            foreach ($quiz['questions'] as $question) {
+                Question::create([
+                    'body' => $question['question'],
+                    'options' => $question['options'],
+                    'answer' => $question['answer'],
+                    'quiz_id' => $quizModel->id
+                ]);
+            }
+        }
     }
 }
